6c1a0f8a4db9f92875b3618e496d6a79815f4c76
LEE-HYUN-JE <LEE-HYUN-JE@users.noreply.github.com>
2026-03-26 11:36:00 +0900

    fix : Topic 알림 수신 설정 HTTP 메서드 POST → PATCH 수정

--- src/main/java/com/example/ajouevent_be_v2/controller/TopicController.java
+++ src/main/java/com/example/ajouevent_be_v2/controller/TopicController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,7 +66,7 @@ public class TopicController implements TopicControllerDocs {
     }
 
     @Override
-    @PostMapping("/api/topic/subscriptions/notification")
+    @PatchMapping("/api/topic/subscriptions/notification")
     public ResponseEntity<Void> updateNotificationPreference(
         @RequestBody TopicNotificationUpdateRequest request, @AuthUser Member member) {
         topicOrchestrator.updateNotificationPreference(request, member);
